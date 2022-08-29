d5b01d671e34b3d11b0f045b1fbd84eeb54986ac
knzt <110175074+knzt@users.noreply.github.com>
2022-08-29 14:11:19 -0300

    configurações do banco de dados inseridas

--- Assets/js/data/config.js
+++ Assets/js/data/config.js
@@ -1,4 +1,9 @@
-
-const firebaseConfig = {}
+const firebaseConfig = {
+apiKey: "AIzaSyDgbNaLaJMfBVOeIVHNocmeTqWK1OwnPRE",
+authDomain: "semana-front-end-b47d7.firebaseapp.com",
+projectId: "semana-front-end-b47d7",
+storageBucket: "semana-front-end-b47d7.appspot.com",
+messagingSenderId: "879535063328",
+appId: "1:879535063328:web:268bd3597f2ca34eff5745"}
 
 export default firebaseConfig
